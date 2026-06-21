726bbcd039a692fb7816b35fd7fac434b55fb09f
Todd Matthews <tmatthews0020@gmail.com>
2026-06-21 15:13:45 -0500

    install nginx controller automatically

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 
   backend "azurerm" {
@@ -23,6 +27,15 @@ provider "azurerm" {
     resource_group {
       prevent_deletion_if_contains_resources = false
     }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
   }
 }
 
---
+++ deploy/terraform/nginx.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "nginx_ingress" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  version          = "4.12.0"
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+    value = "/healthz"
+  }
+
+  depends_on = [azurerm_kubernetes_cluster.main]
+}
